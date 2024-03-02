d75aeb7e0ab007b1d4f712b1dcf902450e07148b
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-01 20:24:23 -0400

    created Header

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+// '../../public/fotoDePerfil3.jpg'
+export default function HeaderPage () {
+  return (
+    <>
+      <header className="head">
+        <div>
+          <Link href='/'>
+            <Image
+              src='/sistemas.png'
+              alt='foto'
+              width={200}
+              height={50}
+            />
+          </Link>
+        </div>
+        <div>
+          <nav className='nav'>
+            <Link href='/solucoes' className='link'>Soluções</Link>
+            <Link href='/integracoes' className='link'>Integrações</Link>
+            <Link href='/planos' className='link'>Planos</Link>
+            <Link href='/segmentos' className='link'>Segmentos</Link>
+            <Link href='/parceiro' className='link'>Seja Parceiro</Link>
+            <Link href='/blog' className='link'>Blog</Link>
+          </nav>
+        </div>
+        <div>
+          <button className='contato'>Experimentar grátis</button>
+        </div>
+      </header>
+    </>
+  )
+}
