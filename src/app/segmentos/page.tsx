7fe5659246091afc7977285a054969550edf6edf
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-05 17:52:19 -0400

    add image in page

--- src/app/segmentos/page.tsx
+++ src/app/segmentos/page.tsx
@@ -1,13 +1,27 @@
 import HeaderPage from '@/components/header'
 import React from 'react'
 import styles from './Segmentos.module.css'
-export default function SegmentosPage() {
+import Image from 'next/image'
+export default function SegmentosPage () {
   return (
     <>
-    <HeaderPage/>
-    <main className={styles.main} >
-
-    </main>
+      <HeaderPage />
+      <main className={styles.main} >
+        <div>
+          <h1 className={styles.h1Segmentos}>
+            Sistema para loja de roupas <br />simples, ágil e acessível
+          </h1>
+          <p className={styles.pSegmentos}>O sistema para loja de roupas do GestãoClick oferece <br/> funcionalidades que atendem às necessidades da sua gestão.<br/> Controle de estoque, gestão de compras, integração com e- <br />commerce, emissão de boletos e notas fiscais, controle de vendas, <br />alertas de estoque mínimo, cadastro de produtos com variações de <br/> cores, tamanhos e muito mais!</p>
+        </div>
+        <div>
+          <Image
+            src='/segmentos.png'
+            alt=''
+            width={500}
+            height={500}
+          />
+        </div>
+      </main>
     </>
   )
 }
