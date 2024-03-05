bb7e17da29e867b2e9cfe93ca0f94e9b561a8668
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-04 21:10:25 -0400

    buttons planos

--- src/app/planos/page.tsx
+++ src/app/planos/page.tsx
@@ -6,6 +6,10 @@ export default function PlanosPage () {
     <>
       <HeaderPage />
       <h1>Nossos planos com desconto</h1>
+      <button className='contato'>Bronze</button>
+      <button className='contato'>Prata</button>
+      <button className='contato'>Ouro</button>
+      <button className='contato'>Platina</button>
     </>
   )
 }
