35907e9bda3d14281ffc3f53f7419664d454506f
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-05 17:31:11 -0400

    background seja parceiro

--- src/app/parceiro/page.tsx
+++ src/app/parceiro/page.tsx
@@ -1,13 +1,13 @@
 import HeaderPage from '@/components/header'
 import React from 'react'
-
+import styles from './Parceiro.module.css'
 export default function ParceiroPage() {
   return (
     <>
     <HeaderPage />
-    <div>
-      Seja Parceiro
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.h1Parceiro}>Mais que parceiros,<br/> somos criadores de <br />oportunidades de <br/> negócio!</h1>
+    </main>
     </>
     
   )
