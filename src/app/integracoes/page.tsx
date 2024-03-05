0412b68fca4834495864a1038b3724e3eaf20afc
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-04 21:03:44 -0400

    mercado livre

--- src/app/integracoes/page.tsx
+++ src/app/integracoes/page.tsx
@@ -1,11 +1,29 @@
 import HeaderPage from '@/components/header'
 import React from 'react'
-
+import styles from './Integracoes.module.css'
+import Image from 'next/image'
 export default function IntegracoesPage() {
   return (
     <>
       <HeaderPage/>
-      Integrações
+      <div className={styles.container}>
+      <div>
+      <h1 className={styles.integracoes_h1}>Sistema de Gestão <br/>
+      integrado ao <br/> Mercado Livre</h1>
+      <p className={styles.pMercado}>O SIS é um Sistema de Gestão integrado ao Mercado <br/> Livre que otimiza a gestão da sua empresa e agiliza o <br/> controle de vendas!</p>
+      <button style={{marginLeft:'4rem',marginTop:'2rem'}}className='contato'>
+        Faça agora o teste de 10 dias grátis!
+      </button>
+      </div>
+      <div>
+      <Image 
+        src='/mercadolivre.png' 
+        alt='Mercado Livre' 
+        width={600}
+        height={500} /> 
+      </div>
+    </div>
+      
     </>
   )
 }
