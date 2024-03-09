6449b7d698ad24ca898f8f50763a54b4c200df6f
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-08 20:34:24 -0400

    link style

--- src/components/form-seg.tsx
+++ src/components/form-seg.tsx
@@ -4,17 +4,15 @@ import React from 'react'
 export default function FormPage () {
   return (
     <>
-   
-   
       <form className='form'>
-        <input type="text" placeholder='Nome da Empresa' /> <br/>
-        <input type="text" placeholder='Seu nome' /> <br/>
+        <input type="text" placeholder='Nome da Empresa' id='empresa' name='empresa' /> <br/>
+        <input type="text" placeholder='Seu nome' id='nome' name='nome' /> <br/>
         <input type="text" placeholder='Telefone' /> <br/>
         <input type="text" placeholder='E-mail' /> <br/>
         <input type="text" placeholder='Sua melhor senha' /><br/> <br/>
         <button className='contato'>COMECE AGORA</button>
       </form>
-      <p className='login'>Já tem uma conta? <Link href='/'>Acessar o sistema</Link></p> 
+      <p className='login'>Já tem uma conta? <a href='/' className='acessar'>Acessar o sistema</a></p> 
     </>
   )
 }
