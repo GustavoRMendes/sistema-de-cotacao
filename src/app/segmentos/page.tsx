aadaa15862a5491de5406d2170ac0437766f61ec
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-08 20:18:52 -0400

    index css

--- src/app/segmentos/page.tsx
+++ src/app/segmentos/page.tsx
@@ -11,7 +11,8 @@ export default function SegmentosPage () {
           <h1 className={styles.h1Segmentos}>
             Sistema para loja de roupas <br />simples, ágil e acessível
           </h1>
-          <p className={styles.pSegmentos}>O sistema para loja de roupas do GestãoClick oferece <br/> funcionalidades que atendem às necessidades da sua gestão.<br/> Controle de estoque, gestão de compras, integração com e- <br />commerce, emissão de boletos e notas fiscais, controle de vendas, <br />alertas de estoque mínimo, cadastro de produtos com variações de <br/> cores, tamanhos e muito mais!</p>
+          <p className={styles.pSegmentos}>O sistema para loja de roupas do SIS Cota oferece <br/> funcionalidades que atendem às necessidades da sua gestão.<br/> Controle de estoque, gestão de compras, integração com e- <br />commerce, emissão de boletos e notas fiscais, controle de vendas, <br />alertas de estoque mínimo, cadastro de produtos com variações de <br/> cores, tamanhos e muito mais!</p>
+          <button className='contato' style={{margin: '2rem 0rem 0rem 2rem'}}>SAIBA MAIS</button>
         </div>
         <div>
           <Image
