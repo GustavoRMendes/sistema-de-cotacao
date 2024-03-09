1b0d152e95e1d77662f459c138701a7643cbd5e7
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-08 20:13:26 -0400

    index css

--- src/app/planos/page.tsx
+++ src/app/planos/page.tsx
@@ -10,6 +10,45 @@ export default function PlanosPage () {
         <button className={styles.planesButtons}>ANUAL</button>
         <button className={styles.planesButtons}>SEMESTRAL</button>
         <button className={styles.planesButtons}>MENSAL</button>
+        <section className={styles.section}>
+          <div className={styles.card}>
+            <div>
+              <h2 className={styles.title}>BRONZE</h2>
+            </div>
+            <p className={styles.economize}>Economize R$ 632</p>
+            <p className={styles.psmall}>R$ <strong className={styles.preco}>79</strong>
+            <br/>
+            por mês no contrato atual
+            </p>
+            <button className={styles.teste}>TESTE GRÁTIS</button> <br/>
+            <a href="#" className={styles.planosLinks}>Ver todos os recursos</a>
+          </div>
+          <div className={styles.card}>
+            <div>
+              <h2 className={styles.title}>PRATA</h2>
+            </div>
+            <p className={styles.economize}>Economize R$ 1.050</p>
+            <p className={styles.psmall}>R$ <strong className={styles.preco}>129</strong>
+            <br/>
+            por mês no contrato atual
+            </p>
+            <button className={styles.teste}>TESTE GRÁTIS</button> <br/>
+            <a href="#" className={styles.planosLinks}>Ver todos os recursos</a>
+          </div>
+          <div className={styles.card}>
+            <div>
+              <h2 className={styles.title}>OURO</h2>
+            </div>
+            <p className={styles.economize}>Economize R$ 1.610</p>
+            <p className={styles.psmall}>R$ <strong className={styles.preco}>199</strong>
+            <br/>
+            por mês no contrato atual
+            </p>
+            <button className={styles.teste}>TESTE GRÁTIS</button> <br/>
+            <a href="#" className={styles.planosLinks}>Ver todos os recursos</a>
+          </div>
+
+        </section>
       </main>
       
     </>
